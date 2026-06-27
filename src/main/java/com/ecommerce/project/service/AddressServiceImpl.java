9fb5c3e6dda69359664ad6add7620b67d99397ba
Ayushuuu <ayushrajput0123@gmail.com>
2026-06-27 18:40:35 +0530

    added orders and payment methods

--- src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -1,4 +1,104 @@
 package com.ecommerce.project.service;
 
-public class AddressServiceImpl {
+import com.ecommerce.project.exception.ResourceNotFoundException;
+import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import com.ecommerce.project.repository.AddressRepository;
+import com.ecommerce.project.repository.UserRepository;
+import com.ecommerce.project.util.AuthUtil;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService{
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public AddressDTO saveUserAddress(AddressDTO addressDTO) {
+        User user = authUtil.loggedInUser();
+        Address address = modelMapper.map(addressDTO, Address.class);
+
+        List<Address> addressList = user.getAddresses();
+        addressList.add(address);
+        user.setAddresses(addressList);
+
+        address.setUser(user);
+        Address savedAddress = addressRepository.save(address);
+
+        return modelMapper.map(savedAddress, AddressDTO.class);
+    }
+
+    @Override
+    public List<AddressDTO> getAllAddresses() {
+        List<Address> addressList = addressRepository.findAll();
+        List<AddressDTO> addressDTOS = addressList
+                .stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .toList();
+        return addressDTOS;
+    }
+
+    @Override
+    public AddressDTO getAddressById(Long addressId) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "Address Id", addressId));
+
+        return modelMapper.map(address, AddressDTO.class);
+    }
+
+    @Override
+    public List<AddressDTO> getUserAddresses() {
+        User user = authUtil.loggedInUser();
+        List<AddressDTO> addressDTOS = user
+                .getAddresses()
+                .stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .toList();
+        return addressDTOS;
+    }
+
+    @Transactional
+    @Override
+    public AddressDTO updateAddress(AddressDTO addressDTO, Long addressId) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "Address Id", addressId));
+        address.setCity(addressDTO.getCity());
+        address.setPincode(addressDTO.getPincode());
+        address.setCountry(addressDTO.getCountry());
+        address.setState(addressDTO.getState());
+        address.setStreet(addressDTO.getStreet());
+        address.setBuildingName(addressDTO.getBuildingName());
+        addressRepository.save(address);
+
+        return modelMapper.map(address, AddressDTO.class);
+    }
+
+    @Override
+    public String deleteAddress(Long addressId) {
+        Address addressFromDB = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "Address Id", addressId));
+
+        User user = addressFromDB.getUser();
+        user.getAddresses().removeIf(address -> address.getAddressId().equals(addressId));
+        userRepository.save(user);
+
+        addressRepository.deleteById(addressId);
+        return "Address with Address Id: " + addressId + " has been deleted successfully!";
+    }
 }
